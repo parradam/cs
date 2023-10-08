d3900143242e4dbccbc0068097175c1b4d36d5d8
Adam Parr <adamparr.uk@gmail.com>
2023-10-08 18:54:19 +0100

    complete Corey Schafer OOP tutorials

---
+++ corey-schafer/Corey Schafer - OOP tutorials.py
@@ -0,0 +1,102 @@
+import datetime
+
+
+class Employee:
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        Employee.num_of_emps += 1
+
+    @property
+    def email(self):
+        return f'{self.first}.{self.last}@company.com'
+
+    @property
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split()
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete name')
+        self.first = None
+        self.last = None
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+    def __repr__(self) -> str:
+        return f"Employee('{self.first}', '{self.last}', '{self.pay}')"
+
+    def __str__(self) -> str:
+        return f'{self.fullname()}, {self.email}'
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
+
+class Developer(Employee):
+    raise_amount = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+emp = Employee('James', 'Jones', 20000)
+emp.fullname = 'Corey Schafer'
+
+print(emp.first)
+print(emp.email)
+print(emp.fullname)
+
+del (emp.fullname)
+print(repr(emp))
